943bc44fa8735c51e84e8b543368737c3d44ec56
zbob889 <zroberts889@gmail.com>
2022-06-27 13:57:13 +0200

    Got some html and css down.  Now I need to figure out the js for making to-do items

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import addElement from './addElement.js';
+import addText from './addText.js';
+
+
+// addElement('div', '', 'toDoWindow', 'container');
+// addElement('div', '', 'toDoLists', 'toDoWindow');
+// addElement('div', '', 'toDoNotes', 'toDoWindow');
+
+// addElement('div', '', 'toDoTitle', 'toDoLists');
+// addElement('p', '', 'toDoTitleText', 'toDoTitle');
+// addElement('div', '', 'toDoSubmit', 'toDoLists');
+// addElement('div', '', 'toDoDisplay', 'toDoLists');
